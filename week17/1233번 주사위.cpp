5b69b8524631cd9673feb69ecca969314a5ef68a
Miseon <71369109+mimi-609@users.noreply.github.com>
2021-07-20 20:03:33 +0900

    1233번 주사위
    2021/5/14 solved

---
+++ week17/1233번 주사위.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+    
+    int a, b, c;
+    cin >> a >> b >> c; // 2 ~ 20, 2 ~ 20, 2 ~ 40
+    // 3 2 3
+
+    int cnt[82] = { 0, };
+
+    for (int i = 1; i <= a; i++) {
+        for (int j = 1; j <= b; j++) {
+            for (int k = 1; k <= c; k++) {
+                cnt[i + j + k]++;
+            }
+        }
+    }
+
+    int max = 1;
+    int result = 0;
+
+    for (int i = 1; i <= 80; i++) {
+        if (cnt[i] > max) {
+            max = cnt[i]; result = i;
+        }
+    }
+
+    cout << result;
+
+}
