5bc569000b987f9855a903a0eca0f18f67b85a0a
Miseon <71369109+mimi-609@users.noreply.github.com>
2021-07-20 19:55:34 +0900

    2858번 기숙사 바닥
    2021/4/2 solved
    - 갈색(B)보다는 계산하기 쉬운 가장자리 빨간색(R) 기준으로 계산하였음
    - R 수를 구할 때는 모서리를 고려해줘야함 (모서리 4개)
    - 직접 그림을 그려서 참고
    - 브루트포스니까 i, j는 대충 5000*5000으로 잡고 처음부터 끝까지 탐색

---
+++ week14/2858번 기숙사 바닥.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+
+	int R, B, L = 0, W = 0;
+	cin >> R >> B;
+
+	for (int i = 1;i<=5000; i++) {
+
+		for (int j = 1;j<=5000; j++) {
+
+			if (i * j == R + B) {
+
+				if ((2 * i) + (2 * (j - 2)) == R) { // R 수 구하기
+					
+					if (i > j) L = i, W = j;
+					else L = j, W = i;
+
+					cout << L << " " << W;
+					return 0;
+
+				}
+
+			}
+
+		}
+
+	}
+	
+}
