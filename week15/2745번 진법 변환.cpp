cc42701d9d8b428f9fd87dc1aedd0fa98075cd27
Miseon <71369109+mimi-609@users.noreply.github.com>
2021-07-20 19:57:13 +0900

    2745번 진법 변환
    2021/5/2 solved
    - 문제를 바로 알아듣고 바로 풀기 어려웠음
    - 진법 변환에 대해서 다시 생각해보게 됨... (너무 바보 같았음 ㅠㅠ)
    - 다음부터 이런 문제는 바로바로 풀 수 있도록 하자!!

---
+++ week15/2745번 진법 변환.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	
+	string N;
+	int B;
+	cin >> N >> B;
+
+	int pow = 1;
+	long long result = 0;
+
+	for (int i = N.length() - 1; i >= 0; i--) {
+
+		int tmpNum;
+		if (N[i] >= '0' && N[i] <= '9') tmpNum = N[i] - '0';
+		else tmpNum = N[i] - 'A' + 10;
+
+		result += tmpNum * pow;
+		pow *= B;
+
+	}
+
+	cout << result;
+
+}
