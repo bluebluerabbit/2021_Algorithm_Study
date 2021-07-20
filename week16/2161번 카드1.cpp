944f92dfcfd6236202abaf4ef424670949331386
Miseon <71369109+mimi-609@users.noreply.github.com>
2021-07-20 20:02:32 +0900

    2161번 카드1
    2021/5/7 solved
    - STL queue 사용

---
+++ week16/2161번 카드1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+queue<int> q;
+
+
+int main() {
+	
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) q.push(i);
+
+	while (!q.empty()) {
+		cout << q.front() << " ";
+		q.pop();
+		
+		int tmp = q.front();
+		q.pop();
+
+		q.push(tmp);
+	}
+
+}
