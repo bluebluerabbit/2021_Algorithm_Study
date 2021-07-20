2a5f7e03bc4b4c4da3b660c55c4965f5e2ed97ce
Miseon <71369109+mimi-609@users.noreply.github.com>
2021-07-20 19:54:42 +0900

    1357번 뒤집힌 덧셈
    2021/4/2 solved
    - 숫자 받기 → 문자열로 바꾸기 → 문자열 뒤집기 → 숫자로 바꾸기 → 계산 후 문자열로 바꿔서 뒤집기 → 숫자로 바꿔서 출력
    - 과정이 헷갈려서 틀린 횟수가 많다.
    - **sprintf** 쓰는 방법을 좀 외우고 다니자

---
+++ week14/1357번 뒤집힌 덧셈.cpp
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main(){
+
+	string a, b;
+	cin >> a >> b;
+
+	string realA, realB;
+
+	for (int i = a.length() - 1; i >= 0; i--) {
+		realA += a[i];
+	}
+
+	for (int i = b.length() - 1; i >= 0; i--) {
+		realB += b[i];
+	}
+
+	int x, y;
+
+	x = stoi(realA);
+	y = stoi(realB);
+
+	char result[1000];
+	string realRE;
+
+	sprintf(result, "%d", x + y);
+
+	for (int i = strlen(result) - 1; i >= 0; i--) {
+		realRE += result[i];
+	}
+
+	cout << stoi(realRE);
+	
+}
